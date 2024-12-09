c2b2c07522ecc8529e746cb4d16d1b8e9dbdf5d0
Adam Szi <szi.adam@simonyi.bme.hu>
2024-12-09 14:04:16 +0100

    fix: basepath for favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>Hydration error by Trailing slash</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={router.basePath + "/favicon.ico"} />
       </Head>
       <Component {...pageProps} />
     </>
