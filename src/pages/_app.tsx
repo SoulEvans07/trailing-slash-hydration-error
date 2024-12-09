4d8d3c001e8c7b9c1c9e0284f1efe49b01e6eebe
Adam Szi <szi.adam@simonyi.bme.hu>
2024-12-09 13:50:35 +0100

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Hydration error by Trailing slash</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ src/pages/other.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function OtherPage() {
+  return (
+    <div>
+      <h1>Othe Page</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
